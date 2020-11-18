d6f12d110dad37ba7faf26313c559b33eeb89c8c
brian chiem <brianfamily5@gmail.com>
2020-11-17 20:37:49 -0700

    Add function to tell age

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+// Challenge one 1: Age in days
+function  ageInDays() {
+    var birthYear = prompt("What year were you born?")
+    var days = (2020 - birthYear) * 365;
+    var h1 = document.createElement("h1");
+    var textAnswer = document.createTextNode("You are " + days + " days old")
+    h1.setAttribute("id", "days")
+    h1.appendChild(textAnswer)
+    document.getElementById('flex-box-result').appendChild(h1)
+} 
