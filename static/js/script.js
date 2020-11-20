4b7c78ab7c87ea68e2c3a8a5498d4dd27c71a8f1
brian chiem <brianfamily5@gmail.com>
2020-11-19 20:49:18 -0700

    Add functions for bot

--- static/js/script.js
+++ static/js/script.js
@@ -23,8 +23,25 @@ function generateDog() {
 function rpsGame(yourChoice) {
     var humanChoice, botChoice;
     // humanChoice = yourChoice.id;
-    // botChoice = 
+    // botChoice = numberToChoice(randToRpsInt())
     // results = decideWinner(humanChoice,botChoice); [0, 1] human | bot won
     // message = finalMessage(results); // "you won"
     rpsFrontEnd(yourChoice.id, botChoice, message);
+}
+
+function randToRpsInt() {
+    return Math.floor(Math.random() * 3)
+}
+
+function numberToChoice(number) {
+    return['rock','paper','scissors'][number]
+}
+
+function decideWinner(yourChoice, computerChoice) {
+    var rpsDatabase = {
+        'rock' : {'scissors': 1, "rock": 0.5, 'paper': 0},
+        'paper' : {'rock': 1, "paper": 0.5, 'scissors': 0},
+        'scissors' : {'paper': 1, "scissors": 0.5, 'rock': 0}
+    }
+    var yourScore = rpsDatabase[yourChoice][computerChoice]
 }
