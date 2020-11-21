2dff27090c310d7a53551d7d62c1b765d0e52ff7
brian chiem <brianfamily5@gmail.com>
2020-11-20 20:22:11 -0700

    Add else if function for buttons

--- static/js/script.js
+++ static/js/script.js
@@ -91,3 +91,11 @@ function rpsFrontEnd (humanImageChoice, botImageChoice, finalMessage) {
     document.getElementById('flex-box-rps-div').appendChild(messageDiv)
     document.getElementById('flex-box-rps-div').appendChild(botDiv);
 }
+
+var all_buttons = document.getElementsByTagName('button');
+console.log(all_buttons)
+
+var copyAllButtons = [];
+for (let i = 0; i < all_buttons.length; i++ ) {
+    copyAllButtons.push(all_buttons[i])
+}
