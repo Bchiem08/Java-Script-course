14ee8cd3103c3c6f3f9b650560cd6ed84ab75037
brian chiem <brianfamily5@gmail.com>
2020-11-23 21:22:59 -0700

    Add functionality to deal button

--- static/js/script.js
+++ static/js/script.js
@@ -155,13 +155,28 @@ const hitSound = new Audio('static/sounds/swish.m4a')
 
 document.querySelector("#blackjack-hit-button").addEventListener("click", blackjackHit)
 
+document.querySelector("#blackjack-deal-button").addEventListener("click", blackjackDeal)
+
 function blackjackHit() {
-    showCard()
+    showCard(YOU);
 }
 
-function showCard() {
+function showCard(activePlayer) {
     let cardImage = document.createElement('img')
     cardImage.src = 'static/images/Q.png'
-    document.querySelector(YOU['div']).appendChild(cardImage)
-    hitSound.play()
+    document.querySelector(activePlayer['div']).appendChild(cardImage)
+    hitSound.play();
+}
+
+function blackjackDeal() {
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img')
+    let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img')
+    
+    for (i=0; i < yourImages.length; i++) {
+        yourImages[i].remove()
+    }
+
+    for (i=0; i < dealerImages.length; i++) {
+        dealerImages[i].remove()
+    }
 }
